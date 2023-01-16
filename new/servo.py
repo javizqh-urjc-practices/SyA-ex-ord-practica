f4fb9e2c7a175b66c043d42b17bcf41ede4278da
Javier Izquierdo Hernandez <j.izquierdoh.2021@alumnos.urjc.es>
2023-01-16 13:36:13 +0100

    Testing

--- new/servo.py
+++ new/servo.py
@@ -7,6 +7,8 @@ import sys, tty, signal, time, pigpio
 import RPi.GPIO as GPIO
 import sensor, actuator
 
+servoPin = 8 # numeracion en modo BCM (que es el usado por defecto por pigpio)
+
 '''
   Según especificaciones de la compañía fabricante de estos servos, Parallax,
   la modulación PWM de estos servos tiene los siguientes rangos:
